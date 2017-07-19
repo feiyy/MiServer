540d62c1d1011ed73659b437d2e6ce7d58b00534
Arc <wc19960408@126.com>
2017-07-19 10:26:01 +0800

    model

--- models/mongo.js
+++ models/mongo.js
@@ -11,7 +11,6 @@ db.once('open', function() {
     });
 
     detailSchema = new Schema({
-        dname: String,
         name: String,
         activity: String,
         brief: String,
@@ -20,53 +19,96 @@ db.once('open', function() {
         rom: String,
         color: String,
         pic: String,
-        price: String,
-        stock: String,
-        urls1: String,
-        urls2: String,
-        urls3: String
+        price: Number,
+        stock: Number,
+        urls1: Object,
+        urls2: Object,
+        urls3: Object
     });
 
     //2. Model 
     userModel = db.model('User', userSchema);
+    detailModel = db.model('Detail', detailSchema);
     // queryUser();
 });
 
-db.addUser = function(user, callback) {
-    //3. Entity
-    var userEntity = new userModel(user);
-    //保存到数据库
-    userEntity.save();
-    callback('success');
+{
+    db.addUser = function(user, callback) {
+        //3. Entity
+        var userEntity = new userModel(user);
+        //保存到数据库
+        userEntity.save();
+        callback('success');
+    }
+
+    db.queryUser = function(callback) {
+        var query = userModel.find({});
+        query.exec(function(err, docs) {
+            callback(docs);
+        });
+    }
+
+    db.queryUserByName = function(name, callback) {
+        userModel.find({ uname: name }, function(err, doc) {
+            callback(doc[0]);
+        })
+    }
+
+    db.queryUserById = function(id, callback) {
+        userModel.findById(id, function(err, doc) {
+            callback(doc[0]);
+        })
+    }
+
+    db.deleteUser = function(id) {
+        userModel.findById(id, function(err, doc) {
+            doc.remove();
+        });
+    }
+
+    db.updateUser = function(id, data) {
+        userModel.findByIdAndUpdate(id, data, function(err, doc) {});
+    }
 }
 
-db.queryUser = function(callback) {
-    var query = userModel.find({});
-    query.exec(function(err, docs) {
-        callback(docs);
-    });
+{
+    db.addDetail = function(Detail, callback) {
+        //3. Entity
+        var DetailEntity = new DetailModel(Detail);
+        //保存到数据库
+        DetailEntity.save();
+        callback('success');
+    }
+
+    db.queryDetail = function(callback) {
+        var query = DetailModel.find({});
+        query.exec(function(err, docs) {
+            callback(docs);
+        });
+    }
+
+    db.queryDetailByName = function(name, callback) {
+        DetailModel.find({ uname: name }, function(err, doc) {
+            callback(doc[0]);
+        })
+    }
+
+    db.queryDetailById = function(id, callback) {
+        DetailModel.findById(id, function(err, doc) {
+            callback(doc[0]);
+        })
+    }
+
+    db.deleteDetail = function(id) {
+        DetailModel.findById(id, function(err, doc) {
+            doc.remove();
+        });
+    }
+
+    db.updateDetail = function(id, data) {
+        DetailModel.findByIdAndUpdate(id, data, function(err, doc) {});
+    }
 }
 
-db.queryUserByName = function(name, callback) {
-    userModel.find({ uname: name }, function(err, doc) {
-        callback(doc[0]);
-    })
-}
-
-db.queryUserById = function(id, callback) {
-    userModel.findById(id, function(err, doc) {
-        callback(doc[0]);
-    })
-}
-
-db.deleteUser = function(id) {
-    userModel.findById(id, function(err, doc) {
-        doc.remove();
-    });
-}
-
-db.updateUser = function(id, data) {
-    userModel.findByIdAndUpdate(id, data, function(err, doc) {});
-}
 
 module.exports = db;
