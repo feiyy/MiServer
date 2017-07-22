3fcc084637b5be988db91526674880d477673426
Arc <wc19960408@126.com>
2017-07-22 11:24:54 +0800

    query id 更正

--- models/mongo.js
+++ models/mongo.js
@@ -63,7 +63,7 @@ db.once('open', function() {
 
     db.queryUserById = function(id, callback) {
         userModel.findById(id, function(err, doc) {
-            callback(doc[0]);
+            callback(doc);
         })
     }
 
