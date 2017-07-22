470001a1ae0b5ea32b92aba86fa377df14f2ff9c
yoyo <2452392450@qq.com>
2017-07-22 15:58:47 +0800

    更新商品对象

--- public/images/details/Air13.3/Air13.3.js
+++ public/images/details/Air13.3/Air13.3.js
@@ -1,9 +1,11 @@
 var detail = {
             name: "小米笔记本Air 13.3英寸",
+            category:"notebook",
             //search页图标，shopcart页图标
        		spic: "/img/Air13.3/spic.webp",
        		//category页图标
        		cpic: "/img/Air13.3/cpic.png",
+       		activity:"",
             brief: "NVIDIA 940MX 独立显卡 / 轻薄全金属机身 / 8GB 内存 + 256GB SSD / 第六代 Intel 酷睿i5 处理器 / FHD 全贴合屏幕 / 高能量密度电池",
             type: [{name: "小米笔记本Air 13.3英寸", color: "silver",pic: "/img/Air13.3/silver.webp", price: "4999", stock: "3"}],
             urls1: [{ url: "/img/Air13.3/url1_1.jpg" }, { url: "/img/Air13.3/url1_2.jpg" }, { url: "/img/Air13.3/url1_3.jpg" }, { url: "/img/Air13.3/url1_4.jpg" }, { url: "/img/Air13.3/url1_5.jpg" }, { url: "/img/Air13.3/url1_6.jpg" }, { url: "/img/Air13.3/url1_7.jpg" }, { url: "/img/Air13.3/url1_8.jpg" }],
