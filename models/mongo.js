c067e8afc6baf8291be7426f0aeec03c77637f15
ffan1069 <1135450954@qq.com>
2017-07-22 11:23:39 +0800

    1111

--- models/mongo.js
+++ models/mongo.js
@@ -3,7 +3,7 @@ var user = {
     uname: "13080868092",
     pwd: "123456",
     hphoto: "/images/account/userimg.png",
-    sex: "male",
+    sex: "male", 
     payment: [],
     address: []
 };
@@ -11,7 +11,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var db = mongoose.createConnection('localhost', 'smallmimall');
 
-db.on('error', function() { console.log("error") });
+db.on('error', function(doc) { console.log(doc) });
 db.once('open', function() {
     //1. Schema
     userSchema = new Schema({
@@ -20,7 +20,8 @@ db.once('open', function() {
         hphoto: String,
         sex: String,
         payment: Array,
-        address: Array
+        address: Array,
+        shoppingcart:Array
     });
 
     detailSchema = new Schema({
@@ -42,10 +43,11 @@ db.once('open', function() {
 {
     db.addUser = function(user, callback) {
         //3. Entity
+        console.log("come in");
         var userEntity = new userModel(user);
         //保存到数据库
         userEntity.save();
-        callback('success');
+        callback('succeed');
     }
 
     db.queryUser = function(callback) {
@@ -96,7 +98,9 @@ db.once('open', function() {
             hphoto: "",
             sex: "男",
             payment: [{ orderId: "5160607962200971", orderState: "已完成", orderItemsPic: [{ "url": "img/orderItem1.jpg" }], orderItemsName: [{ "name": "小米手环 2 黑色" }], orderDate: "2016/04/22 17:46", orderPayMethod: "微信支付", orderBuyer: "张三", orderRecDate: "2016/04/23 17:46", orderRecAddr: "辽宁省沈阳市浑南新区东北大学浑南校区", orderItemNum: "1", orderItemMoney: "149" }],
-            address: []
+            address: [],
+            shoppingcart:[{goodsId:"1",goodsName:"小米随身wifi",goodsPrice:"19.00",goodsCount:"2",url:"/img/mi63.jpg"},
+                          {goodsId:"2",goodsName:"米兔故事机",goodsPrice:"149.00",goodsCount:"4",url:"/img/mi62.jpg"}]
         };
         db.addUser(address, function(cb) {
             callback(cb);
@@ -143,26 +147,26 @@ db.once('open', function() {
     db.updateDetail = function(id, data) {
         DetailModel.findByIdAndUpdate(id, data, function(err, doc) {});
     }
-    db.init = function(callback) {
-        var detail = {
-            name: "小米6",
-            activity: "7月14日早10点，小米6 64GB 亮白色 首卖",
-            brief: "变焦双摄，4 轴防抖 / 骁龙835 旗舰处理器，6GB 大内存，最大可选128GB 闪存 / 5.15吋 护眼屏 / 四曲面玻璃/陶瓷机身",
-            type: [{ name: "陶瓷尊享版", ram: "6GB", rom: "128GB", color: "亮黑色", pic: "/img/lightblack.jpg", price: "2999", stock: "0" },
-                { name: "全网通版", ram: "6GB", rom: "64GB", color: "亮黑色", pic: "/img/lightblack.jpg", price: "2499", stock: "2" },
-                { name: "陶瓷尊享版", ram: "6GB", rom: "128GB", color: "亮白色", pic: "/img/lightwhite.jpg", price: "2899", stock: "3" },
-                { name: "全网通版", ram: "6GB", rom: "128GB", color: "亮黑色", pic: "/img/lightblack.jpg", price: "2899", stock: "5" },
-                { name: "陶瓷尊享版", ram: "6GB", rom: "64GB", color: "亮蓝色", pic: "/img/lightblue.jpg", price: "2499", stock: "0" },
-                { name: "陶瓷尊享版", ram: "6GB", rom: "64GB", color: "亮白色", pic: "/img/lightwhite.jpg", price: "2499", stock: "9" }
-            ],
-            urls1: [{ url: "/img/mi61.jpg" }, { url: "/img/mi62.jpg" }, { url: "/img/mi63.jpg" }, { url: "/img/mi64.jpg" }, { url: "/img/mi65.jpg" }, { url: "/img/mi66.jpg" }, { url: "/img/mi67.jpg" }, { url: "/img/mi68.jpg" }, { url: "/img/mi69.jpg" }, { url: "/img/mi610.jpg" }, { url: "/img/mi611.jpg" }, { url: "/img/mi612.jpg" }, { url: "/img/mi613.jpg" }, { url: "/img/mi614.jpg" }, { url: "/img/mi615.jpg" }],
-            urls2: [{ url: "/img/parameter1.jpg" }, { url: "/img/parameter2.jpg" }, { url: "/img/parameter3.jpg" }, { url: "/img/parameter4.jpg" }],
-            urls3: [{ url: "/img/yushou.jpg" }]
-        };
-        db.addDetail(detail, function(cb) {
-            callback(cb);
-        });
-    }
+    // db.init = function(callback) {
+    //     var detail = {
+    //         name: "小米6",
+    //         activity: "7月14日早10点，小米6 64GB 亮白色 首卖",
+    //         brief: "变焦双摄，4 轴防抖 / 骁龙835 旗舰处理器，6GB 大内存，最大可选128GB 闪存 / 5.15吋 护眼屏 / 四曲面玻璃/陶瓷机身",
+    //         type: [{ name: "陶瓷尊享版", ram: "6GB", rom: "128GB", color: "亮黑色", pic: "/img/lightblack.jpg", price: "2999", stock: "0" },
+    //             { name: "全网通版", ram: "6GB", rom: "64GB", color: "亮黑色", pic: "/img/lightblack.jpg", price: "2499", stock: "2" },
+    //             { name: "陶瓷尊享版", ram: "6GB", rom: "128GB", color: "亮白色", pic: "/img/lightwhite.jpg", price: "2899", stock: "3" },
+    //             { name: "全网通版", ram: "6GB", rom: "128GB", color: "亮黑色", pic: "/img/lightblack.jpg", price: "2899", stock: "5" },
+    //             { name: "陶瓷尊享版", ram: "6GB", rom: "64GB", color: "亮蓝色", pic: "/img/lightblue.jpg", price: "2499", stock: "0" },
+    //             { name: "陶瓷尊享版", ram: "6GB", rom: "64GB", color: "亮白色", pic: "/img/lightwhite.jpg", price: "2499", stock: "9" }
+    //         ],
+    //         urls1: [{ url: "/img/mi61.jpg" }, { url: "/img/mi62.jpg" }, { url: "/img/mi63.jpg" }, { url: "/img/mi64.jpg" }, { url: "/img/mi65.jpg" }, { url: "/img/mi66.jpg" }, { url: "/img/mi67.jpg" }, { url: "/img/mi68.jpg" }, { url: "/img/mi69.jpg" }, { url: "/img/mi610.jpg" }, { url: "/img/mi611.jpg" }, { url: "/img/mi612.jpg" }, { url: "/img/mi613.jpg" }, { url: "/img/mi614.jpg" }, { url: "/img/mi615.jpg" }],
+    //         urls2: [{ url: "/img/parameter1.jpg" }, { url: "/img/parameter2.jpg" }, { url: "/img/parameter3.jpg" }, { url: "/img/parameter4.jpg" }],
+    //         urls3: [{ url: "/img/yushou.jpg" }]
+    //     };
+    //     db.addDetail(detail, function(cb) {
+    //         callback(cb);
+    //     });
+    // }
 }
 
 module.exports = db;
