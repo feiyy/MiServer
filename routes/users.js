e782eaebabd1ecbcdb056fbc1e8c7bc2c34cb9cb
ffan1069 <1135450954@qq.com>
2017-07-23 17:11:48 +0800

    package

--- routes/users.js
+++ routes/users.js
@@ -63,38 +63,6 @@ router.post('/uploader', function(req, res) {
   });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
@@ -197,14 +165,9 @@ router.post('/changepwd', function(req, res, next) {
     }
 });
 
-<<<<<<< HEAD
-
-
-=======
 router.get('/logout', function(req, res, next) {
     delete req.session.user;
     res.render('index', { fragment: 1 });
 });
->>>>>>> 1103f391854b41ecb8bca4fb4b89399d5868a463
 
 module.exports = router;
